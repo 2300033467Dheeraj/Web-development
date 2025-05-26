e3346123cfdf8dc5706a97b14c4eb90e42911590
𝐃 𝐇 𝐄 𝐄 𝐑 𝐀 𝐉 <2300033467@kluniversity.in>
2025-05-26 22:10:21 +0530

    javascript basics

---
+++ Practicefolder/Java script/Arthimetic operations.js
@@ -0,0 +1,34 @@
+let a = 10;
+let b = 20;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a ** b); // Exponentiation operator
+console.log(a++); // Postfix increment
+console.log(++a); // Prefix increment
+console.log(a--); // Postfix decrement
+console.log(--a); // Prefix decrement\
+console.log(a == b); // Equality operator
+console.log(a != b); // Inequality operator
+console.log(a > b); // Greater than operator
+console.log(a < b); // Less than operator
+console.log(a >= b); // Greater than or equal to operator
+console.log(a <= b); // Less than or equal to operator
+console.log(a === b); //
+console.log(a !== b); //
+console.log(a && b); // Logical AND
+console.log(a || b); // Logical OR
+console.log(!a); // Logical NOT
+console.log(a & b); // Bitwise AND
+console.log(a | b); // Bitwise OR
+console.log(a ^ b); // Bitwise XOR
+console.log(~a); // Bitwise NOT
+console.log(a << 1); // Bitwise left shift
+console.log(a >> 1); // Bitwise right shift
+console.log(a >>> 1); // Bitwise zero-fill right shift
+console.log(a && b); // Logical AND
+console.log(a || b); // Logical OR
+console.log(!a); // Logical NOT
+console.log(a & b); // Bitwise AND
