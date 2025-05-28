2da0ebec7d9b9ccf0ffb5f71cd89bdc87229e586
𝐃 𝐇 𝐄 𝐄 𝐑 𝐀 𝐉 <2300033467@kluniversity.in>
2025-05-28 23:41:23 +0530

    complete blackjack game

--- Practicefolder/Java script/self recap/blackjack game/blackjack in browser.js
+++ Practicefolder/Java script/self recap/blackjack game/blackjack in browser.js
@@ -1,23 +1,28 @@
 let num1=11;
-let num2=11;
+let num2=10;
 let sum = num1 + num2;
 let hasBlackjack = false
 let isAlive = true
 let message =""
-function startgame(){
-    
-}
+messageEl = document.getElementById("message-el")
+console.log(messageEl)
+sumEL = document.getElementById("sum-el")
+console.log(sumEL)
+cardEl = document.getElementById("cards-el")
+console.log(cardEl)
+function startGame(){
 
-if (sum < 21) {
-    message = "Do you want to draw a new card?"
+    if (sum < 21) {
+        message = "Do you want to draw a new card?"
+    }
+    else if (sum === 21) {
+        message ="You've got Blackjack!"
+        hasBlackjack = true
+    } else {
+        message ="You're out of the game!"
+        isAlive = false;
+    }
+    messageEl.textContent = message;
+    sumEL.textContent = "Sum: " + sum;
+    cardEl.textContent = "Cards: " + num1 + ", " + num2;
 }
-else if (sum === 21) {
-    message ="You've got Blackjack!"
-    hasBlackjack = true
-} else {
-    message ="You're out of the game!"
-    isAlive = false;
-}
-console.log(message);
-console.log("does have black jack: " +hasBlackjack);
-console.log("is he in game: "+ isAlive);
