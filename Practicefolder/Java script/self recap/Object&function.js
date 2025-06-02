b235794dd3dbaacc3fe80bcfa5f54266f0da6f52
𝐃 𝐇 𝐄 𝐄 𝐑 𝐀 𝐉 <2300033467@kluniversity.in>
2025-06-02 16:15:44 +0530

    updates

---
+++ Practicefolder/Java script/self recap/Object&function.js
@@ -0,0 +1,11 @@
+let mydetails ={
+    name:"Dheeraj",
+    age: 19,
+    county : "India",
+}
+function getDetails() {
+    return mydetails.name + " is " + mydetails.age + " years old and lives in " + mydetails.county;
+}
+
+
+console.log(getDetails()) 
---
+++ Practicefolder/Java script/self recap/Queue.js
@@ -0,0 +1,11 @@
+// practiceing push pop shift unshift splice
+let arr=['Dheeraj', 'Saqib', 'Surya', 'shashank']
+console.log(arr);
+arr.push('Sahil');
+console.log(arr);
+arr.pop(2);
+console.log(arr);
+arr.shift();
+console.log(arr);
+arr.unshift('Sahil');
+console.log(arr);
