66ed15d280f18f569ce853146465024f524aea1f
𝐃 𝐇 𝐄 𝐄 𝐑 𝐀 𝐉 <2300033467@kluniversity.in>
2025-06-03 23:53:48 +0530

    new css file

--- Practicefolder/Java script/sample project/extention.js
+++ Practicefolder/Java script/sample project/extention.js
@@ -1,25 +1,24 @@
 let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
-let ulEl = document.getElementById("ul-el");
-console.log(ulEL)
+const ulEl = document.getElementById("ul-el");
 inputBtn.addEventListener("click", function(){
-    myLeads.push(inputEl.value);
-    inputEl.value = "";
-    renderLeads();
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    renderLeads()
 
 })
 function renderLeads() {
     let listItem = " ";
     for (let i = 0; i < myLeads.length; i++) {
-        ulEL.innerHTML += 
-        `
+        listItem += 
+        `   
             <li>
                 <a target = '_blank' href='${myLeads[i]}'>
                 ${myLeads[i]}
                 </a>
             </li>
-        ` ;
+        `
     }
     ulEl.innerHTML = listItem;
 }
