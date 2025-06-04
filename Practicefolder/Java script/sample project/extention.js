3ba7eb684a2cdab3d6e80041c66e23490426614a
𝐃 𝐇 𝐄 𝐄 𝐑 𝐀 𝐉 <2300033467@kluniversity.in>
2025-06-04 14:47:58 +0530

    localstorage

--- Practicefolder/Java script/sample project/extention.js
+++ Practicefolder/Java script/sample project/extention.js
@@ -2,6 +2,7 @@ let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
+localStorage.setItem("myLeads",inputEl.value)
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
     inputEl.value = ""
